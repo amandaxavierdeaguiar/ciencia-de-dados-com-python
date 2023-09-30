8008945ef554518d17cc5907c1de400c88362b71
amandaxavierdeaguiar <amandaxavierdeaguiar@gmail.com>
2023-09-30 02:11:39 +0100

    desafio

--- Desafios/equilibrando_saldo.py
+++ Desafios/equilibrando_saldo.py
@@ -7,25 +7,3 @@ resultado = (saldo_atual + valor_deposito) - valor_retirada
 
 resultado = (saldo_atual + valor_deposito) - valor_retirada
 print(f'Saldo atualizado na conta: {resultado:.1f}')
-
-
-
-
-
-
-
-#def saldo(saldo_atual, valor_depositado, valor_retirada):
-   # if (valor_depositado > 0):
-  #      saldo_atual = (saldo_atual + valor_deposito) - valor_retirada
-    #    print('Saldo atual: € {:.2f}'.format)
-    #elif (valor_retirada >= saldo_atual):
-   #     print('Saldo indisponivel')
-
-#def calcular_saldo(saldo_atual, valor_depositado, valor_retirada):
-    #if (valor_depositado > 0) and ((saldo_atual + valor_depositado)):
-        #print('Saldo atual: € {:.2f}'.format((saldo_atual+valor_depositado)))
-    #elif (saldo_atual >= valor_retirada)
-       # print('Saldo atual: € {:.2f}'.format((saldo_atual-valor_depositado)))
-#TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
-
-#TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
